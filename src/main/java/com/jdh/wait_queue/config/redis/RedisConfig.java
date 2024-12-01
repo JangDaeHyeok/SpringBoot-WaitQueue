779d545e64a88f279eb991260477641fc1f4d3da
JangDaeHyeok <jangdaehyeog@gmail.com>
2024-12-01 22:27:24 +0900

    chore: 불필요 import 제거

--- src/main/java/com/jdh/wait_queue/config/redis/RedisConfig.java
+++ src/main/java/com/jdh/wait_queue/config/redis/RedisConfig.java
@@ -3,16 +3,10 @@ package com.jdh.wait_queue.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
